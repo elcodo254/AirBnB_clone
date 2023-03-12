421ec489630e1d62db31a8e6821b351f5c7c6ee6
elcodo254 <ellawachaga@gmail.com>
2023-03-12 14:18:31 -0700

    create and update user.py file

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Define User class that inherits from BaseModel"""
+
+
+class User:
+    """Represents class user
+    
+    Args:
+        email (str): email of the user
+        password (str): user password
+        first_name (str): first name of the user
+        last_name (str): last name of user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
